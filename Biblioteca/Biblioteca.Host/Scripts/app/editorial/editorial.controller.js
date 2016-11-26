3138cbf7a75d6528a13bcbbfada16d492e9c11ea
Alumno <Alumno@AULA-PC>
2016-11-26 14:50:54 -0600

    Agregando controlador y vista editoriales

---
+++ Biblioteca/Biblioteca.Host/Scripts/app/editorial/editorial.controller.js
@@ -0,0 +1,24 @@
+﻿app.controller(
+    'editorialController',
+    [
+        '$scope',
+        function ($scope) {
+            $scope.editoriales = [{
+                id: '1',
+                nombre: 'editorial 1'
+            },
+            {
+                id: '2',
+                nombre: 'editorial 2'
+            },
+            {
+                id: '3',
+                nombre: 'editorial 3'
+            }];
+            $scope.editorialActual = {
+                id: '123',
+                nombre:'editorial 123'
+            };
+        }
+    ]
+    );
--- Biblioteca/Biblioteca.Host/Scripts/app/routes.js
+++ Biblioteca/Biblioteca.Host/Scripts/app/routes.js
@@ -1,12 +1,20 @@
 ﻿app.config(['$routeProvider',
 function ($routerProvider) {
-    $routerProvider.when('/', {
-        templateUrl: "/Scripts/app/home/home.template.html",
-        controller: "homeController"
-    })
-    .otherwise({
-        redirectTo:'/'
-    })
+    $routerProvider
+        .when('/', {
+            templateUrl: "/Scripts/app/home/home.template.html",
+            controller: "homeController"
+        })
+        .otherwise({
+            redirectTo:'/'
+        })
+        .when('/editoriales', {
+            templateUrl: "/Scripts/app/editorial/editorial.template.html",
+            controller: "editorialController"
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
 }
 
 
